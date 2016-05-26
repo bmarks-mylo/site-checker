8dd279fee517a5e9db45f7747d1a153a39d44e12
bmarks-mylo <bmarks@locktonaffinity.com>
2016-05-26 10:54:14 -0500

    moved to single file

--- check.js
+++ check.js
@@ -12,11 +12,10 @@ let sites = {
 
 //for each base, combine w/ brand, do request, output status
 _.each(sites, (brands, base) => {
-    console.log('Site: ' + base);
     _.each(brands, (brand) => {
         let uri = base + '/' + brand;
-       request(uri, (err, msg) => {
-          console.log(uri + '\t\t' + msg.statusCode);
-       });
+        request(uri, (err, msg) => {
+            console.log(`${uri}\t\t${msg.statusCode}`);
+        });
     });
 });
